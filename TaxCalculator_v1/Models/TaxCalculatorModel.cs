dc16325d42858437739f860dbab2c374c780a340
wandab <wanda.brightsiyanda@gmail.com>
2017-07-23 23:00:13 +0200

    First Push

---
+++ TaxCalculator_v1/Models/TaxCalculatorModel.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxCalculator_v1.Models
+{
+    public class TaxCalculatorModel
+    {
+        public DataTable _deductionTable;
+        public DataTable _ageDeductionTable;
+
+        public TaxCalculatorModel()
+        {
+            _deductionTable = GetDeductionTable();
+            _ageDeductionTable = AgeBasedTable();
+        }
+
+
+        private static DataTable GetDeductionTable()
+        {
+            // Here we create a DataTable with four columns.
+            DataTable table = new DataTable();
+            table.Columns.Add("Min", typeof(int));
+            table.Columns.Add("Max", typeof(int));
+            table.Columns.Add("Deduction", typeof(double));
+
+            // Here we add five DataRows.
+            table.Rows.Add(5000, 10000, 5.0);
+            table.Rows.Add(10001, 20000, 7.5);
+            table.Rows.Add(20001, 35000, 9.0);
+            table.Rows.Add(35001, 50000, 15.0);
+            table.Rows.Add(50001, 70000, 25.0);
+            table.Rows.Add(70001, int.MaxValue, 30.0);
+
+            return table;
+        }
+
+        private static DataTable AgeBasedTable()
+        {
+            // Here we create a DataTable with four columns.
+            DataTable table = new DataTable();
+            table.Columns.Add("MinAge", typeof(double));
+            table.Columns.Add("MaxAge", typeof(double));
+            table.Columns.Add("Relief", typeof(double));
+            table.Columns.Add("% Deduction", typeof(double));
+
+
+            // Here we add five DataRows.
+            table.Rows.Add(18, 50, 2000, 100);
+            table.Rows.Add(50, int.MaxValue, 5000,85);
+
+            return table;
+        }
+    }
+}
---
+++ TaxCalculator_v1/ViewModels/TaxCalculatorViewModel.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using TaxCalculator_v1.ICommands;
+using TaxCalculator_v1.Models;
+
+namespace TaxCalculator_v1.ViewModels
+{
+    class TaxCalculatorViewModel : ViewModelBase
+    {
+
+        private double _salary;
+        private int _age;
+        private double _deductionTableValue;
+        private double _ageDeductionTableRelief;
+        private double _ageDeductionTablePercentageDeduction;
+        private double _finalTaxValue;
+
+        private readonly ObservableCollection<string> _history = new ObservableCollection<string>();
+        public TaxCalculatorModel _taxCalModel;
+        public DataTable _deductionTableVM;
+        public DataTable _ageDeductionTableVM;
+
+        public TaxCalculatorViewModel()
+        {
+            _taxCalModel = new TaxCalculatorModel();
+            _deductionTableVM = _taxCalModel._deductionTable;
+            _ageDeductionTableVM = _taxCalModel._ageDeductionTable;
+        }
+
+        public string FinalTaxValue
+        {
+            get { return _finalTaxValue.ToString(); }
+        }
+        public IEnumerable<string> History
+        {
+            get { return _history; }
+        }
+
+        public double Salary
+        {
+            get { return _salary; }
+            set
+            {
+                _salary = value;
+                RaisePropertyChangedEvents("Salary");
+            }
+        }
+
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                _age = value;
+                RaisePropertyChangedEvents("Age");
+            }
+        }
+
+        public ICommand CalculateTaxCommand
+        {
+            get { return new DelegateCommand(TaxCalculator); }
+        }
+
+        private void TaxCalculator()
+        {
+
+            foreach (DataRow row in _deductionTableVM.Rows)
+            {
+                if (_salary >= row.Field<int>(0) && _salary <= row.Field<int>(1))
+                {
+                    _deductionTableValue = row.Field<double>(2);
+                    break;
+                }
+            }
+
+            foreach (DataRow ageRow in _ageDeductionTableVM.Rows)
+            {
+                if (_age >= ageRow.Field<double>(0) && _age <= ageRow.Field<double>(1))
+                {
+                    _ageDeductionTableRelief = ageRow.Field<double>(2);
+                    _ageDeductionTablePercentageDeduction = ageRow.Field<double>(3);
+                    break;
+                }
+            }
+
+            _finalTaxValue = (_salary - _ageDeductionTableRelief) * ((_ageDeductionTablePercentageDeduction/100.0)*(_deductionTableValue)/100);
+            AddToHistory(_finalTaxValue);
+        }
+
+        private void AddToHistory(double t)
+        {
+            _history.Clear();
+            _history.Add("Your Tax to pay is: " + t.ToString());
+
+        }
+    }
+}
